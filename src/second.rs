bd5f826a754a9f68eead250dd7bf586f1bdbd27a
Roman Gershman <romange@gmail.com>
2021-06-20 14:03:20 +0300

    Add third.rs

--- src/second.rs
+++ src/second.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 pub struct List<T> {
     head: Link<T>,
 }
@@ -8,6 +10,10 @@ pub struct IntoIter<T>(List<T>);
 
 pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -49,26 +55,30 @@ impl<T> List<T> {
     }
 
     pub fn peek(&self) -> Option<&T> {
-        self.head.as_ref().map(|boxed_node| {
-            &boxed_node.elem
-        })
+        self.head.as_ref().map(|boxed_node| &boxed_node.elem)
     }
 
     pub fn peek_mut(&mut self) -> Option<&mut T> {
-        self.head.as_mut().map(|node| {
-            &mut node.elem
-        })
+        self.head.as_mut().map(|node| &mut node.elem)
     }
 
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
 
-     // We declare a fresh lifetime here for the *exact* borrow that
+    // We declare a fresh lifetime here for the *exact* borrow that
     // creates the iter. Now &self needs to be valid as long as the
     // Iter is around.
     pub fn iter<'a>(&'a self) -> Iter<'a, T> {
-        Iter { next: self.head.as_ref().map(|node| &**node), }
+        Iter {
+            next: self.head.as_ref().map(|node| node.deref()),
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
     }
 }
 
@@ -79,7 +89,6 @@ impl<T> Iterator for IntoIter<T> {
         self.0.pop()
     }
 }
-
 
 impl<'a, T> Iterator for Iter<'a, T> {
     type Item = &'a T;
@@ -92,12 +101,25 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
+}
+
 #[test]
 fn peek() {
     let mut list = List::new();
     assert_eq!(list.peek(), None);
     assert_eq!(list.peek_mut(), None);
-    list.push(1); list.push(2); list.push(3);
+    list.push(1);
+    list.push(2);
+    list.push(3);
 
     assert_eq!(list.peek(), Some(&3));
     assert_eq!(list.peek_mut(), Some(&mut 3));
@@ -109,15 +131,47 @@ fn peek() {
     assert_eq!(list.pop(), Some(42));
 }
 
+#[cfg(test)]
+mod test {
+    use super::List;
 
-#[test]
-fn into_iter() {
-    let mut list = List::new();
-    list.push(1); list.push(2); list.push(3);
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
-    let mut iter = list.into_iter();
-    assert_eq!(iter.next(), Some(3));
-    assert_eq!(iter.next(), Some(2));
-    assert_eq!(iter.next(), Some(1));
-    assert_eq!(iter.next(), None);
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+    }
 }
